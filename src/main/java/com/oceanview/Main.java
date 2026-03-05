f1efd6fc7914ad6869b8b1c179eba89a4893b975
Yasiru <yasirujayasinghe15th@gmail.com>
2026-03-06 00:50:48 +0530

    The CORS issue was resolved.

--- src/main/java/com/oceanview/Main.java
+++ src/main/java/com/oceanview/Main.java
@@ -1,9 +1,8 @@
 package com.oceanview;
 
-
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import com.oceanview.controller.AuthController;
-
 
 import java.net.InetSocketAddress;
 
@@ -14,12 +13,32 @@ public class Main {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
         // Auth
-        server.createContext("/api/register", (new AuthController()));
-        server.createContext("/api/login", (new AuthController()));
+        server.createContext("/api/register", wrapWithCORS(new AuthController()));
+        server.createContext("/api/login", wrapWithCORS(new AuthController()));
 
         server.setExecutor(null);
         server.start();
 
         System.out.println("Ocean View Resort Server Started on port 8000 ✅");
+    }
+
+    public static HttpHandler wrapWithCORS(HttpHandler handler) {
+        return exchange -> {
+
+            if ("OPTIONS".equalsIgnoreCase(exchange.getRequestMethod())) {
+                exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+                exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+                exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
+                exchange.sendResponseHeaders(204, -1);
+                exchange.close();
+                return;
+            }
+
+            exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+            exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
+
+            handler.handle(exchange);
+        };
     }
 }
